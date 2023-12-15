3768ac4371b876ea53d96598be4df6c6888a0654
pr_dagi <dagimtheteencoder@gmail.com>
2023-12-15 05:58:54 +0300

    playwright sync_api imported to the proto

--- prototype.py
+++ prototype.py
@@ -1,27 +1,38 @@
+from playwright.sync_api import sync_playwright, Page, Browser, Route
+
 class NovelScraper:
-    def __init__(self, novel_page_url, scraping_starting_chapter, download_folder_location) -> None:
-        pass
-    # playwright utitls
-    ## start_playwright
-    def _start_playwright() -> pw:
+    def __init__(self, novel_page_url, start_chapter, download_folder_location) -> None:
+        self.novel_page_url = novel_page_url
+        self.scraping_starting_chapter = scraping_starting_chapter
+        self.download_folder_location = download_folder_location
+
+    # 1. Playwright Utilities
+
+    # 1.1 Start Playwright
+    def _start_playwright(self) -> None:
+        #TODO...log info
         self.playwright = sync_playwright().start()
 
-    # lanuch browser
-    def _launch_browser() -> browser:
+    # 1.2 Launch Browser
+    def _launch_browser(self) -> None:
+        #TODO...log info
         self.browser = self.playwright.chromium.launch()
 
-    # initialize the page
-    def _initialize_page() -> page:
+    # 1.3 Initialize the Page
+    def _initialize_page(self) -> None:
+        #TODO...log info
         context = self.browser.new_context()
 
         # Open a new page
         page = context.new_page()
         page.set_default_timeout(0)
-        page.route("**/*", self._unload_unwanted_res)
+        page.route("**/*", self._unload_unwanted_resources)  # Corrected method name
 
-    # unload unwanted resoucresce from the bg
-    def _unload_unwanted_res():
-        # resource type to be blocked. e.g. image, stylesheet
+        self.page = page
+
+    # 1.4 Unload Unwanted Resources from the Background
+    def _unload_unwanted_resources(self, route: Route) -> None:
+        # Resource types to be blocked, e.g., image, stylesheet
         BLOCK_RESOURCE_TYPES = [
             "beacon",
             "csp_report",
@@ -33,8 +44,8 @@ class NovelScraper:
             "texttrack",
             # "stylesheet",
         ]
-        
-        # block popular 3rd party resources like tracking and advertisements.
+
+        # Block popular 3rd-party resources like tracking and advertisements.
         BLOCK_RESOURCE_NAMES = [
             "service.supercounters.com",
             "analytics",
@@ -46,16 +57,23 @@ class NovelScraper:
             "googletagmanager",
             "woafoame"
         ]
-        
+
         if route.request.resource_type in BLOCK_RESOURCE_TYPES:
-            #....log her
+            #TODO... log info
             return route.abort()
         if any(key in route.request.url for key in BLOCK_RESOURCE_NAMES):
-            #...log her
+            #TODO ... log info
             return route.abort()
 
         return route.continue_()
-    
-    def _stop_playwright() -> None:
-        #https://playwright.dev/python/docs/api/class-playwright#playwright-stop
+
+    # 1.5 Close the Browser
+    def _close_browser(self) -> None:
+        #TODO...log info
+        self.browser.close()
+
+    # 1.6 Stop Playwright
+    def _stop_playwright(self) -> None:
+        # https://playwright.dev/python/docs/api/class-playwright#playwright-stop
+        #TODO...log info
         self.playwright.stop()
