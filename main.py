db8cf90c78c24956681c43051ba767ffb25eab92
Dagim Alemu <142475159+progdagi@users.noreply.github.com>
2024-01-09 18:07:20 +0000

    test dublicate-issue-1

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from src.novel_scraper import NovelScraper
 
 scraper = NovelScraper()
 
-novel_page_url = "https://sangtacviet.vip/truyen/faloo/1/1378270/"#input("novel page url: ")
+issue_novel_page_url = "https://sangtacviet.vip/truyen/faloo/1/1378270/"#input("novel page url: ")
 starting_chapter = 1#input("starting chapter to scrap: ")
 download_folder_path = "download"#input("Download Folder Path: ")
 
 
 if __name__ == "__main__":
-    scraper.scrap(novel_page_url, starting_chapter, download_folder_path)
+    scraper.scrap(issue_novel_page_url, starting_chapter, download_folder_path)
