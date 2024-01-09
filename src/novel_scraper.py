6efe8b7d9b5634dc37ba490e4c48c71281964cbd
Dagim Alemu <142475159+progdagi@users.noreply.github.com>
2024-01-09 02:20:55 +0000

    test.encode

--- src/novel_scraper.py
+++ src/novel_scraper.py
@@ -11,10 +11,10 @@ import os
 import sys
 
 # Set UTF-8 as the global output encoder
-os.device_encoding()
-os.environ["PYTHONUTF8OUTPUT"] = "1"
+# os.device_encoding()
+# os.environ["PYTHONUTF8OUTPUT"] = "1"
 
-print("-----------test encoding: ", sys.stdout.encoding)
+# print("-----------test encoding: ", sys.stdout.encoding)
 
 # Third-Party Library Imports
 import pytesseract
